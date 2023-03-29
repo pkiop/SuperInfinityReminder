fdf8716625e5fbf50d6cb1fcfed11f425ae84be6
pkiop <pkiopb@gmail.com>
2023-03-30 05:21:30 +0900

    Feat: style refactoring

---
+++ src/Icons/index.ts
@@ -0,0 +1,4 @@
+export {default as CheckIcon} from './check.svg';
+export {default as CrossIcon} from './cross.svg';
+export {default as AddIcon} from './add.svg';
+export {default as TrashIcon} from './trash.svg';
---
+++ src/styles/color.ts
@@ -0,0 +1,34 @@
+const primary = {
+  0: '#000000',
+  10: '#21005D',
+  20: '#381E72',
+  30: '#4F378B',
+  40: '#6750A4',
+  50: '#7F67BE',
+  80: '#D0BCFF',
+  90: '#EADDFF',
+};
+
+const secondary = {
+  0: '#000000',
+  10: '#1D192B',
+  20: '#332D41',
+  30: '#4A4458',
+  40: '#625B71',
+  50: '#7A7289',
+  60: '#958DA5',
+  70: '#B0A7C0',
+  80: '#CCC2DC',
+  90: '#E8DEF8',
+};
+
+const color = {
+  primary,
+  secondary,
+  appBackground: primary[90],
+  contentBackground: secondary[70],
+  white: '#FFFFFF',
+  black: '#000000',
+};
+
+export default color;
